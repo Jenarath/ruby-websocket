61d4b4025919c0adac93ab83e52dd717e810edb6
Jenarath <>
2024-12-23 13:17:58 +0700

    fix websocket

--- websocket_server.rb
+++ websocket_server.rb
@@ -2,5 +2,5 @@ require "socketify"
 
 Socketify::App.new()
 .get("/", lambda {|response, request| response.end("Hello World socketify from Ruby!")})
-.listen(8082, lambda {|config| puts "Listening on port #{config.port}" })
+.listen(8081, lambda {|config| puts "Listening on port #{config.port}" })
 .run()
