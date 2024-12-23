e5648e8bd0933cebc99b1e1e2d79e29cde22b1c0
Jenarath <>
2024-12-23 13:24:51 +0700

    websocket

--- websocket_server.rb
+++ websocket_server.rb
@@ -1,6 +1,51 @@
-require "socketify"
+require 'socket'
+require 'websocket'
 
-Socketify::App.new()
-.get("/", lambda {|response, request| response.end("Hello World socketify from Ruby!")})
-.listen(8081, lambda {|config| puts "Listening on port #{config.port}" })
-.run()
+class WebSocketServer
+  def initialize(port)
+    @server = TCPServer.new(port)
+    @clients = []
+  end
+
+  def start
+    loop do
+      socket = @server.accept
+      Thread.new(socket) do |client|
+        handshake = WebSocket::Handshake::Server.new
+        frame = WebSocket::Frame::Incoming::Server.new
+
+        while (data = client.gets)
+          handshake << data
+          if handshake.finished?
+            client.write handshake.to_s
+            break
+          end
+        end
+
+        @clients << client
+
+        while (data = client.gets)
+          frame << data
+          while (message = frame.next)
+            puts "Received message: #{message}"
+            broadcast(message)
+          end
+        end
+
+        @clients.delete(client)
+        client.close
+      end
+    end
+  end
+
+  def broadcast(message)
+    @clients.each do |client|
+      frame = WebSocket::Frame::Outgoing::Server.new(data: message, type: :text)
+      client.write frame.to_s
+    end
+  end
+end
+
+# Start the WebSocket server
+server = WebSocketServer.new(8081)
+server.start
