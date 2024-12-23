317ee775d9c338d6962ffbf02ab67eab9e586ddc
Jenarath <>
2024-12-23 13:11:22 +0700

    fix websocket

--- websocket_server.rb
+++ websocket_server.rb
@@ -6,31 +6,30 @@ class WebSocketServer
     EM.run do
       @clients = []
 
-      # Start a TCP server
-      EM.start_server('0.0.0.0', 8081, nil) do |connection|
-        # Initialize the WebSocket
+      # Start the server
+      EM.start_server('0.0.0.0', 8081, Faye::WebSocket::Connection) do |connection|
+        # Use the driver properly
         ws = Faye::WebSocket.new(connection)
 
-        ws.on(:open) do |event|
+        ws.on :open do |event|
           puts 'Client connected'
           @clients << ws
         end
 
-        ws.on(:message) do |event|
+        ws.on :message do |event|
           puts "Received message: #{event.data}"
           # Broadcast the message to all connected clients
           @clients.each { |client| client.send(event.data) }
         end
 
-        ws.on(:close) do |event|
+        ws.on :close do |event|
           puts 'Client disconnected'
           @clients.delete(ws)
-          ws = nil
         end
       end
     end
   end
 end
 
-# Start the server
+# Start the WebSocket server
 WebSocketServer.start
